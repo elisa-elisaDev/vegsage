44df55a54d8b1a50867f76e9487c2e5aba9cf7c5
Andreas Kissner <K@kwebdesign.space>
2026-03-11 14:09:55 +0100

    Update legal operator information

--- lib/legalConfig.ts
+++ lib/legalConfig.ts
@@ -4,8 +4,8 @@
  */
 
 export const legalConfig = {
-  OPERATOR_NAME: process.env.OPERATOR_NAME ?? "To be completed",
-  OPERATOR_ADDRESS: process.env.OPERATOR_ADDRESS ?? "To be completed",
+  OPERATOR_NAME: process.env.OPERATOR_NAME ?? "Leïla Kissner",
+  OPERATOR_ADDRESS: process.env.OPERATOR_ADDRESS ?? "Rue du Champ-Fort 6, 2853 Courfaivre",
   OPERATOR_COUNTRY: process.env.OPERATOR_COUNTRY ?? "Switzerland",
   CONTACT_EMAIL: process.env.CONTACT_EMAIL ?? "contact.vegsage@gmail.com",
   GOVERNING_LAW: process.env.GOVERNING_LAW ?? "Switzerland",
